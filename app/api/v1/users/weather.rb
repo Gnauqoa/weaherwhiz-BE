86f4fe2b4cb19a77afac60f3119ce4ed8a008f1b
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-05 16:51:42 +0700

    Add phone and birth to user API and user serializer.

--- app/api/v1/users/weather.rb
+++ app/api/v1/users/weather.rb
@@ -2,7 +2,7 @@ module V1
   module Users
     class Weather < Base
       namespace :weather do
-        resources :forecast
+        resources :forecast do
           desc 'Get weather forecast', summary: "Get weather forecast"
           params do
             requires :q, type: String, desc: "Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name."
@@ -11,6 +11,7 @@ module V1
           get do
             format_response(Weathers::Forecast.new(params).call)
           end
+        end
       end
     end
   end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :birth, :email, :first_name, :last_name, :created_at, :updated_at
+  attributes :id, :username, :phone, :birth, :email, :first_name, :last_name, :created_at, :updated_at
 end
