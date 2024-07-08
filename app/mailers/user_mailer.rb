3e673f9d7e3dcc42c504360217bd7817859d767d
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-08 15:25:02 +0700

    Add verify mail.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   def verify_email(user, verification)
     @user = user
     @verification = verification
+    @FE_URL = ENV["FE_URL"]
     mail(to: user.email, subject: "Verify your account")
   end
 end
