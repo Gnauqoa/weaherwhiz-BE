f7ecc4a81490079630e62139d568fab1d1a33be8
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-08 16:29:57 +0700

    Add config email for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,19 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.raise_delivery_errors = true
+  host = "#{ENV["FE_URL"]}"
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["MAIL_ACCOUNT"],
+    :password             => ENV["MAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
