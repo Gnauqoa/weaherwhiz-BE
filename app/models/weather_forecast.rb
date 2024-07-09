880aa71a8c33f0d2bf58472cd9203b1aef68c3ba
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-09 15:52:13 +0700

    Add WeatherForecast document.

---
+++ app/models/weather_forecast.rb
@@ -0,0 +1,10 @@
+class WeatherForecast
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :location, type: Object
+  field :current, type: Object
+  field :forecast, type: Object
+  field :location_id, type: String 
+  field :date, type: Date
+end
