9b74ea92a063147f34642fbc5ef5f4ed829128d3
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-09 15:52:33 +0700

    Add caching forecast data by location_id and date.

--- app/services/v1/weathers/forecast.rb
+++ app/services/v1/weathers/forecast.rb
@@ -8,11 +8,28 @@ module V1
       end
   
       def call
+        query = AutoComplete.new(q: params[:q]).call.success
+
+        forecasts = WeatherForecast.where(
+                              location_id: 2717932, 
+                              date: Date.current
+        ).to_a
+        return Success(forecasts.first) if forecasts.length > 0
+
         params_arr = params.sort.to_h.map { |k, v| "#{k}=#{v}" }.sort
         params_str = params_arr.join('&')
-        HTTParty.get(
+        result = HTTParty.get(
           "#{ENV["WEATHER_API_HOST"]}/forecast.json?#{params_str}"
         )
+
+        WeatherForecast.create!(
+          location: result["location"],
+          current: result["current"],
+          forecast: result["forecast"],
+          location_id: query["id"],
+          date: Date.current
+        )
+        Success(result)
       end
     end
   end
