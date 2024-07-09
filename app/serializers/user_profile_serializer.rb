8f61077d6af4c81a59849f47a7b34975914b31ae
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-09 16:56:15 +0700

    Update user serializer.

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserProfileSerializer < ActiveModel::Serializer
-  attributes :id, :username, :notification_each_day, :location_id, :phone, :birth, :email, :first_name, :last_name, :created_at, :updated_at
+  attributes :id, :username, :notification_each_day, :location_id, :location_query, :phone, :birth, :email, :first_name, :last_name, :created_at, :updated_at
 end
